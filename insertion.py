4f87b579c24599039e0fd3226daadb4db1c6d1f7
hodaya12 <hodayaroz1@gmail.com>
2023-08-21 15:42:25 +0300

    add documentation to insertion.py

--- insertion.py
+++ insertion.py
@@ -2,6 +2,13 @@ import trie
 import os
 path = "Archive"
 def insert_to_dic(file_path:str,count_uid:int)->(dict,int):
+    """
+    insert the lines of the file into a dictionary that the keys are uid of the line
+    and the values are tuples of the file path and the sentence itself
+    :param file_path: the file
+    :param count_uid: the next uid of line to use
+    :return the dictionary and the uid of the next line:
+    """
     with open(file_path, 'r') as file:
         file_content_list = file.readlines()
     dict_of_lines_in_file={}
@@ -11,6 +18,14 @@ def insert_to_dic(file_path:str,count_uid:int)->(dict,int):
     return dict_of_lines_in_file,count_uid
 
 def insert_to_tree(file_path:str,trie_tree_of_all_words:trie.Trie,count_uid:int)->(trie.Trie,int):
+    """
+    insert the words from the file to trie tree
+
+    :param file_path: the file
+    :param trie_tree_of_all_words: trie tree to insert the words to
+    :param count_uid: the next uid of line to use
+    :return the trie tree and the uid of the next line:
+    """
     with open(file_path, 'r') as file:
         file_content_list = file.readlines()
     for line in file_content_list:
@@ -24,6 +39,13 @@ def insert_to_tree(file_path:str,trie_tree_of_all_words:trie.Trie,count_uid:int)
 
 
 def insert(file_paths:list)->(dict,trie.Trie):
+    """
+    insert all the words in all the files into the trie tree and all the lines into the dictionary
+    that its keys are the uid of the lines and its values are tuples of the file path and the sentence itself
+
+    :param file_paths:list of file paths
+    :return:the dictionary of all sentences and the tree of all words
+    """
     dic_of_all_sentences={}
     trie_tree_of_all_words = trie.Trie()
     count_id=1
