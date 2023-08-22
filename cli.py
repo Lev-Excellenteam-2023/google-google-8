1ab2c5e49d39c895943c1ea33aff90d03e2ad68c
TamariIv <taivgi@g.jct.ac.il>
2023-08-22 10:15:01 +0300

    fix prints in cli

--- cli.py
+++ cli.py
@@ -12,7 +12,7 @@ def main():
     print("Start typing and press enter to get suggestions\nPress Ctrl+c to kill program :)\n")
     while True:
         prefix = input("Type......\n")
-        [print(sentence) for sentence in get_best_k_completions(prefix)]
+        [print(sentence.completed_sentence) for sentence in get_best_k_completions(prefix)]
 
 
 if __name__ == '__main__':
